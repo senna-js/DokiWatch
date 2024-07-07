d60c5f1558b36ad81f68d8096e0719ab037ebd27
Eshan276 <eshandas2002@gmail.com>
2024-07-08 01:39:55 +0530

    chore: get anilist code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import SearchIcon from '@mui/icons-material/Search';
-import Sidebar from './components/sidebar'; // Import the Sidebar component
+import React, { useState, useEffect } from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import Sidebar from "./components/sidebar"; // Import the Sidebar component
 
 const Navbar = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [profilePic, setProfilePic] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
+  const [profilePic, setProfilePic] = useState("");
+  useEffect(() => {
+    const queryParams = new URLSearchParams(window.location.search);
+    if (queryParams.has("code")) {
+      const codeValue = queryParams.get("code");
+      console.log("Code:", codeValue);
+    }
+  }, []);
 
   useEffect(() => {
     const fetchAniListProfile = async () => {
@@ -20,21 +27,21 @@ const Navbar = () => {
       `;
 
       try {
-        const response = await fetch('https://graphql.anilist.co', {
-          method: 'POST',
+        const response = await fetch("https://graphql.anilist.co", {
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
+            "Content-Type": "application/json",
+            Accept: "application/json",
           },
           body: JSON.stringify({
-            query
+            query,
           }),
         });
 
         const { data } = await response.json();
         setProfilePic(data.User.avatar.large);
       } catch (error) {
-        console.error('Error fetching AniList profile:', error);
+        console.error("Error fetching AniList profile:", error);
       }
     };
 
@@ -45,24 +52,37 @@ const Navbar = () => {
     setSearchTerm(event.target.value);
   };
 
-  const handleSearchKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
+  const handleSearchKeyDown = (
+    event: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (event.key === "Enter") {
       executeSearch();
     }
   };
 
   const executeSearch = () => {
-    console.log('Searching for:', searchTerm);
+    console.log("Searching for:", searchTerm);
     // Implement your search logic here
-  
+
     // Clear the search term state after executing the search
-    setSearchTerm('');
+    setSearchTerm("");
   };
 
   // Define the SVG component
   const DefaultProfileIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-10 w-10 text-gray-500">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="h-10 w-10 text-gray-500"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+      />
     </svg>
   );
 
