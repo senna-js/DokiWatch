d5dd40288e957ddeb39fe51de5c79e75a11b5971
karan8404 <kkawathia8404@gmail.com>
2024-07-09 01:41:06 +0530

    feat: Improve styling of AnimeStack component and add horizontal line separator

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -129,13 +129,13 @@ export const Navbar = () => {
       </div>
       <div>
       </div>
-      <div className="flex items-center">
-
+      <div className="flex gap-4 items-center">
+        <DefaultProfileIcon />
         <SignedIn>
           <div>
             {profilePic ?
               <img src={profilePic || ""} alt="Profile" className="h-10 w-10 rounded-full" />
-              : <DefaultProfileIcon />}
+              : <></>}
           </div>
           <SignOutButton />
         </SignedIn>
