4f90f69ffe115abaa0ea573ab4e858e03b4f90df
karan8404 <kkawathia8404@gmail.com>
2024-07-11 02:31:06 +0530

    refactor: Update AdvancedSearch component to handle genre selection and search functionality

--- src/components/AdvancedSearch.tsx
+++ src/components/AdvancedSearch.tsx
@@ -1,15 +1,19 @@
 import { MdOutlineCheckBox, MdOutlineCheckBoxOutlineBlank, MdOutlineIndeterminateCheckBox } from 'react-icons/md'
 import { useState } from 'react'
+import { Button } from '@mui/material'
 
 const Bullet = (props: BulletProps) => {
     const [bulletType, setBulletType] = useState<BulletType>(BulletType.indeterminate)
     const handleClick = () => {
         if (bulletType === BulletType.enabled) {
             setBulletType(BulletType.disabled)
+            props.handleGenreSelection(props.genreIndex, -1)
         } else if (bulletType === BulletType.disabled) {
             setBulletType(BulletType.indeterminate)
+            props.handleGenreSelection(props.genreIndex, 0)
         } else {
             setBulletType(BulletType.enabled)
+            props.handleGenreSelection(props.genreIndex, 1)
         }
     }
 
@@ -17,7 +21,7 @@ const Bullet = (props: BulletProps) => {
 
     return (
         <div onClick={handleClick} className='flex hover:cursor-pointer items-center gap-1'>
-            {props.text}
+            <p className='select-none'>{props.text}</p>
             {bulletType === BulletType.enabled && <MdOutlineCheckBox />}
             {bulletType === BulletType.indeterminate && <MdOutlineCheckBoxOutlineBlank />}
             {bulletType === BulletType.disabled && <MdOutlineIndeterminateCheckBox />}
@@ -28,13 +32,16 @@ const Bullet = (props: BulletProps) => {
 export const AdvancedSearch = (props: AdvancedSearchProps) => {
     return (
         <div className='backdrop-blur-lg flex'>
-            <div className='container m-6 p-6 rounded-lg'>
-                <div >
-                    <h1 className='text-center text-bold font-poppins text-2xl'>Genres</h1>
-                    <hr className='my-2' />
-                    <div className='flex flex-wrap gap-4'>
-                        {props.genres.map(genre => <Bullet key={genre} text={genre} />)}
-                    </div>
+            <div className='container m-6 p-6 rounded-lg flex flex-col gap-4'>
+                <h1 className='text-center text-bold font-poppins text-2xl'>Genres</h1>
+                <hr className='my-2' />
+                <div className='flex flex-wrap gap-4'>
+                    {props.genres.map((genre, index) =>
+                        <Bullet key={genre} text={genre} genreIndex={index} handleGenreSelection={props.handleGenreSelection} />
+                    )}
+                </div>
+                <div className='flex justify-center'>
+                    <Button onClick={props.handleSearch} variant='contained'>Search</Button>
                 </div>
             </div>
         </div>
@@ -49,8 +56,12 @@ enum BulletType {
 
 type BulletProps = {
     text: string
+    genreIndex: number
+    handleGenreSelection: (index: number, set: number) => void
 }
 
 type AdvancedSearchProps = {
     genres: string[]
+    handleGenreSelection: (index: number, set: number) => void
+    handleSearch: () => void
 }
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -6,38 +6,78 @@ import { AnimeCard } from "../components/AnimeCard"
 import { AnimeData } from "../interfaces/AnimeData"
 import { AdvancedSearch } from "../components/AdvancedSearch"
 
-const genres = ["Action", "Adventure", "Comedy", "Drama","Ecchi", "Fantasy", "Horror","Mahou Shoujo", "Mecha","Music", "Mystery", "Psychological", "Romance", "Sci-Fi", "Slice of Life", "Sports", "Supernatural", "Thriller"]
+const genres = ["Action", "Adventure", "Comedy", "Drama", "Ecchi", "Fantasy", "Horror", "Mahou Shoujo", "Mecha", "Music", "Mystery", "Psychological", "Romance", "Sci-Fi", "Slice of Life", "Sports", "Supernatural", "Thriller"]
 
 export const Search = () => {
     let [searchParams, setSearchParams] = useSearchParams();
-    var searchTerm, genreTerm, term;
+    const [searchTrigger, setSearchTrigger] = useState<boolean>(false);
+    var searchTerm: any, genreTerm, genreNotTerm, term;
     const [anime, setAnime] = useState<AnimeData[]>([])
+    const [genreSelections, setGenreSelections] = useState<number[]>(Array(genres.length).fill(0));
+
+    const handlegenreSelection = (index: number, set: number) => {
+        console.log("Index: " + index + " Set: " + set, "Genre : " + genres[index])
+        let newSelections = [...genreSelections];
+        newSelections[index] = set;
+        setGenreSelections(newSelections);
+    }
+
+    const handleSearch = () => {
+        var genreString = "";
+        var genreNotString = "";
+        for (var i = 0; i < genreSelections.length; i++) {
+            if (genreSelections[i] === 1) {
+                genreString += genres[i] + ",";
+            }
+            else if (genreSelections[i] === -1) {
+                genreNotString += genres[i] + ",";
+            }
+        }
+
+        genreString = genreString.slice(0, -1);
+        genreNotString = genreNotString.slice(0, -1);
+        console.log("Search Term: " + searchTerm)
+        console.log("Genre String: " + genreString)
+        console.log("Genre Not String: " + genreNotString)
+
+        const params = new URLSearchParams();
+        params.append("genre", genreString);
+        params.append("genreNot", genreNotString);
+        if (searchTerm !== undefined && searchTerm !== "" && searchTerm !== null) {
+            params.append("search", searchTerm);
+        }
+
+        setSearchParams(params);
+        setSearchTrigger(!searchTrigger);
+    }
 
     useEffect(() => {
         searchTerm = searchParams.get("search")
         genreTerm = searchParams.get("genre")
+        genreNotTerm = searchParams.get("genreNot")
+
+        term = ""
+        if (searchTerm) {
+            term = `search: "${searchTerm}",`
+        }
         if (genreTerm) {
             genreTerm = genreTerm.split(',').map(genre => `"${genre.trim()}"`);
             genreTerm = `[${genreTerm.join(',')}]`;
+            term = term + `genre_in: ${genreTerm},`
         }
+        if (genreNotTerm) {
+            genreNotTerm = genreNotTerm.split(',').map(genre => `"${genre.trim()}"`);
+            genreNotTerm = `[${genreNotTerm.join(',')}]`;
+            term = term + `genre_not_in: ${genreNotTerm},`
+        }
+        if(term == "")
+            term = ","
 
-        if (searchTerm && genreTerm) {
-            term = `search: "${searchTerm}", genre_in: ["${genreTerm}"]`
-        } else if (searchTerm) {
-            term = `search: "${searchTerm}"`
-        }
-        else if (genreTerm) {
-            term = `genre_in: ${genreTerm}`
-        }
-        else {
-            term = ``
-        }
-
-        console.log(term.toString())
+        console.log("Searched term : " + term.toString())
 
         var query = `{
                         Page(page: 1, perPage: 20) {
-                            media(${term}, sort: POPULARITY_DESC,type: ANIME) {
+                            media(${term} sort: POPULARITY_DESC,type: ANIME) {
                                 idMal
                                 title {
                                     romaji
@@ -67,9 +107,9 @@ export const Search = () => {
         }).then(response => {
             // Log the data to the console
             console.log(response.data);
-            setAnime(response.data.data.Page.media.map((item:any) => ({
+            setAnime(response.data.data.Page.media.map((item: any) => ({
                 mal_id: item.idMal,
-                title : {
+                title: {
                     romaji: item.title.romaji,
                     english: item.title.english
                 },
@@ -77,15 +117,15 @@ export const Search = () => {
                     large: item.coverImage.extraLarge,
                     color: item.coverImage.color
                 }
-              })));
+            })));
         }).catch(error => {
             console.error(error);
         });
-    }, [searchParams])
+    }, [searchTrigger])
 
     return (
-        <div>
-            <AdvancedSearch genres={genres} />
+        <div className="flex flex-col items-center justify-center">
+            <AdvancedSearch genres={genres} handleGenreSelection={handlegenreSelection} handleSearch={handleSearch} />
             <Stack spacing={2} direction="row" flexWrap="wrap">
                 {
                     anime.map((anime) => (
