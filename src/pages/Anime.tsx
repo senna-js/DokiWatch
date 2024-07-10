338f43b3a656d181591a9ca30fe7b2a861308d34
Ankit Das <ankitdasxisc@gmail.com>
2024-07-10 14:28:13 +0530

    refactor: Add eslint-disable for @typescript-eslint/no-explicit-any in Anime.tsx

--- src/pages/Anime.tsx
+++ src/pages/Anime.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useParams, useNavigate } from "react-router-dom"
 import axios from "axios"
 import { useEffect, useState } from "react"
