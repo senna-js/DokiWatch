9a371bbeec1b33454468c0477be74a8fc128b599
Ankit Das <ankitdasxisc@gmail.com>
2024-07-11 20:50:59 +0530

    refactor: Add lazy loading for images in Anime.tsx

--- src/pages/Anime.tsx
+++ src/pages/Anime.tsx
@@ -3,13 +3,18 @@ import { useParams, useNavigate } from "react-router-dom"
 import axios from "axios"
 import { useEffect, useState } from "react"
 import Content from "../components/ReadMore"
+import { ANIME } from "@consumet/extensions"
 
 export const Anime = () => {
     const params = useParams()
     const navigate = useNavigate()
     const [animeData, setAnimeData] = useState<any>()
     const [userRating, setUserRating] = useState(0);
+    const gogoanime = new ANIME.Gogoanime();
 
+    gogoanime.search("One Piece").then(data => {
+        console.log(data);
+    })
 
     const handleRating = (ratingValue: number) => {
         setUserRating(ratingValue);
@@ -22,7 +27,7 @@ export const Anime = () => {
                 console.log(res.data)
                 setAnimeData(res.data.data)
             })
-            
+
     }, [params.id])
 
     const handleWatch = () => {
