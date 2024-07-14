25e8645bccd501d4f864fad5035119b5c24e2df2
karan8404 <kkawathia8404@gmail.com>
2024-07-15 01:06:41 +0530

    refactor: Add proxy configuration for API requests

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    proxy:{
+      '/api':{
+        target:'https://ec.netmagcdn.com:2228/hls-playback/',
+        changeOrigin:true,
+        rewrite:(path)=>path.replace(/^\/api/,'')
+      }
+    }
+  }
 })
