5c73ff4934e3d46ee2254b97e5897b76bd6ee4cd
karan8404 <kkawathia8404@gmail.com>
2024-07-15 17:52:30 +0530

    refactor: Update API routing and status codes in api.ts

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -4,12 +4,12 @@ import serverless from "serverless-http";
 const api = express();
 
 const router = Router();
-router.get("/a", (req, res) => res.send("api/a/master.m3u8"));
-router.get("/b", (req, res) => res.send("api/b/master.m3u8"));
-router.get("/c", (req, res) => res.send("api/c/master.m3u8"));
-router.get("/d", (req, res) => res.send("api/d/master.m3u8"));
-router.get("/e", (req, res) => res.send("api/e/master.m3u8"));
-router.get("/f", (req, res) => res.send("api/f/master.m3u8"));
+router.get("/a", (req, res) => res.status(201));
+router.get("/b", (req, res) => res.status(202));
+router.get("/c", (req, res) => res.status(203));
+router.get("/d", (req, res) => res.status(204));
+router.get("/e", (req, res) => res.status(205));
+router.get("/f", (req, res) => res.status(206));
 
 api.use("/api/", router);
 
